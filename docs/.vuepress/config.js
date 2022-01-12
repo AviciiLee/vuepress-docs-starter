d470fa8fd049ff820b969cd18c8b64afd290877f
AshinLee <lx_ashin@aliyun.com>
2022-01-12 09:16:53 +0800

    组件库文档提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+const { path } = require('@vuepress/utils')
+
+module.exports = {
+  title: 'fe-docs',
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.png',
+    navbar: [
+      {
+        text: '组件库',
+        children: [
+          {
+            text: '业务组件',
+            children: ['/components/table.md'],
+          },
+        ],
+      }
+    ],
+  },
+  plugins: [
+    [
+      '@vuepress/register-components',
+      {
+        componentsDir: path.resolve(__dirname, './components')
+      },
+    ],
+  ]
+}
